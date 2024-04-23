c82421c2c6cf45ebdffc2ca837566e1c0f710afb
samramariyam <120046251+samramariyam@users.noreply.github.com>
2024-04-23 11:56:36 +0530

    Add files via upload

---
+++ Quizz App/script.js
@@ -0,0 +1,259 @@
+const progressBar = document.querySelector(".progress-bar"),
+  progressText = document.querySelector(".progress-text");
+
+const progress = (value) => {
+  const percentage = (value / time) * 100;
+  progressBar.style.width = `${percentage}%`;
+  progressText.innerHTML = `${value}`;
+};
+
+const startBtn = document.querySelector(".start"),
+  numQuestions = document.querySelector("#num-questions"),
+  category = document.querySelector("#category"),
+  difficulty = document.querySelector("#difficulty"),
+  timePerQuestion = document.querySelector("#time"),
+  quiz = document.querySelector(".quiz"),
+  startScreen = document.querySelector(".start-screen");
+
+let questions = [],
+  time = 30,
+  score = 0,
+  currentQuestion,
+  timer;
+
+const startQuiz = () => {
+
+  let username = document.querySelector("#username").value;
+  if (username === "") {
+    alert("Please enter your name");
+    return;
+  }
+
+  localStorage.setItem("username", username);
+
+  loadingAnimation();
+  const url = `http://localhost:8080/questions/topic?topic=javascript`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      questions = data;
+      console.log(questions)
+      setTimeout(() => {
+        startScreen.classList.add("hide");
+        quiz.classList.remove("hide");
+        currentQuestion = 1;
+        showQuestion(questions[0]);
+      }, 1000);
+    });
+};
+
+startBtn.addEventListener("click", startQuiz);
+
+const showQuestion = (question) => {
+  const questionText = document.querySelector(".question"),
+    answersWrapper = document.querySelector(".answer-wrapper");
+  questionNumber = document.querySelector(".number");
+
+  questionText.innerHTML = question.question;
+
+  const answers = [
+    ...question.options.filter(e => e != question.answer),
+    question.answer,
+  ];
+
+  // const answers = question.options; 
+  answersWrapper.innerHTML = "";
+  answers.sort(() => Math.random() - 0.5);
+  answers.forEach((answer) => {
+    answersWrapper.innerHTML += `
+                  <div class="answer ">
+            <span class="text">${answer}</span>
+            <span class="checkbox">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+        `;
+  });
+
+  questionNumber.innerHTML = ` Question <span class="current">${
+    questions.indexOf(question) + 1
+  }</span>
+            <span class="total">/${questions.length}</span>`;
+  //add event listener to each answer
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.addEventListener("click", () => {
+      if (!answer.classList.contains("checked")) {
+        answersDiv.forEach((answer) => {
+          answer.classList.remove("selected");
+        });
+        answer.classList.add("selected");
+        submitBtn.disabled = false;
+      }
+    });
+  });
+
+  time = timePerQuestion.value;
+  startTimer(time);
+};
+
+const startTimer = (time) => {
+  timer = setInterval(() => {
+    if (time === 3) {
+      playAdudio("countdown.mp3");
+    }
+    if (time >= 0) {
+      progress(time);
+      time--;
+    } else {
+      checkAnswer();
+    }
+  }, 1000);
+};
+
+const loadingAnimation = () => {
+  startBtn.innerHTML = "Loading";
+  const loadingInterval = setInterval(() => {
+    if (startBtn.innerHTML.length === 10) {
+      startBtn.innerHTML = "Loading";
+    } else {
+      startBtn.innerHTML += ".";
+    }
+  }, 500);
+};
+function defineProperty() {
+  var osccred = document.createElement("div");
+  osccred.innerHTML =
+    "A Project By <a href='https://github.com/samramariyam/Quiz-App' target=_blank>Samra Mariyam</a>";
+  osccred.style.position = "absolute";
+  osccred.style.bottom = "0";
+  osccred.style.right = "0";
+  osccred.style.fontSize = "10px";
+  osccred.style.color = "#ccc";
+  osccred.style.fontFamily = "sans-serif";
+  osccred.style.padding = "5px";
+  osccred.style.background = "#fff";
+  osccred.style.borderTopLeftRadius = "5px";
+  osccred.style.borderBottomRightRadius = "5px";
+  osccred.style.boxShadow = "0 0 5px #ccc";
+  document.body.appendChild(osccred);
+}
+
+defineProperty();
+
+const submitBtn = document.querySelector(".submit"),
+  nextBtn = document.querySelector(".next");
+submitBtn.addEventListener("click", () => {
+  checkAnswer();
+});
+
+nextBtn.addEventListener("click", () => {
+  nextQuestion();
+  submitBtn.style.display = "block";
+  nextBtn.style.display = "none";
+});
+
+const checkAnswer = () => {
+  clearInterval(timer);
+  const selectedAnswer = document.querySelector(".answer.selected");
+  if (selectedAnswer) {
+    const answer = selectedAnswer.querySelector(".text").innerHTML;
+    console.log(currentQuestion);
+    if (answer === questions[currentQuestion - 1].answer) {
+      score++;
+      selectedAnswer.classList.add("correct");
+    } else {
+      selectedAnswer.classList.add("wrong");
+      const correctAnswer = document
+        .querySelectorAll(".answer")
+        .forEach((answer) => {
+          if (
+            answer.querySelector(".text").innerHTML ===
+            questions[currentQuestion - 1].answer
+          ) {
+            answer.classList.add("correct");
+          }
+        });
+    }
+  } else {
+    const correctAnswer = document
+      .querySelectorAll(".answer")
+      .forEach((answer) => {
+        if (
+          answer.querySelector(".text").innerHTML ===
+          questions[currentQuestion - 1].answer
+        ) {
+          answer.classList.add("correct");
+        }
+      });
+  }
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.classList.add("checked");
+  });
+
+  submitBtn.style.display = "none";
+  nextBtn.style.display = "block";
+};
+
+const nextQuestion = () => {
+  if (currentQuestion < questions.length) {
+    currentQuestion++;
+    showQuestion(questions[currentQuestion - 1]);
+  } else {
+    showScore();
+  }
+};
+
+const endScreen = document.querySelector(".end-screen"),
+  finalScore = document.querySelector(".final-score"),
+  totalScore = document.querySelector(".total-score");
+  const leaderboard = document.querySelector(".leaderboard");
+const showScore = () => {
+  endScreen.classList.remove("hide");
+  quiz.classList.add("hide");
+  finalScore.innerHTML = score;
+  totalScore.innerHTML = `/ ${questions.length}`;
+
+
+  // call the api to update the score
+
+  const reqdata = {
+    score: score,
+    username: localStorage.getItem("username"),
+    topic: 'javascript',
+    date: "Not_Available",
+  };
+
+  fetch('http://localhost:8080/leaderboard/add', { method: 'POST', body: JSON.stringify(reqdata), headers: { 'Content-Type' : "application/json"}}).then(res => res.json());
+
+  // now we need to get the score from the api
+  fetch('http://localhost:8080/leaderboard/get?topic=javascript')
+    .then(resp => resp.json())
+      .then(respJson =>{
+
+        // sort the array based on the score
+        const leaderboardRes = respJson.sort((a, b) => b.score - a.score);
+
+        // now we need to display the leaderboard
+        console.log(leaderboardRes);
+
+        // iterate and generate html for leaderboard
+        let lString = ""
+        leaderboardRes.forEach(e => {
+            lString += `<li>${e.username} - ${e.score}</li>`
+        })
+
+        leaderboard.innerHTML = lString;
+      })
+};
+
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+const playAdudio = (src) => {
+  const audio = new Audio(src);
+  audio.play();
+};
